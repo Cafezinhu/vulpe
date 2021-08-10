b940d3952b9b2a455ace16ad3b94b469bdb52b77
Aerton <aertoncastro@gmail.com>
2021-08-10 18:43:38 -0300

    furro de volta

--- src/roupas/pecas/corpo/corpoFurro.ts
+++ src/roupas/pecas/corpo/corpoFurro.ts
@@ -1,7 +1,7 @@
 import type { Peca } from "../../../types/peca";
 
 export const corpoFurro: Peca = {
-    image: "/images/corpo-Furro.png",
+    image: "/images/corpo-furro.png",
     nome: "Corpo Furro",
     // buttonColor: "#fea94b"
 }
