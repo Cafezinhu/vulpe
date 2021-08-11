5e2c7cae73da6aaff30ae975bb3eb51432236593
Aerton <aertoncastro@gmail.com>
2021-08-11 12:42:50 -0300

    dino

--- src/roupas/partes/torso.ts
+++ src/roupas/partes/torso.ts
@@ -1,5 +1,7 @@
 import { torsoStore } from "../../stores/pecaStore";
 import type { Parte } from "../../types/parte";
+import { cozinheira } from "../pecas/torso/cozinheira";
+import { dino } from "../pecas/torso/dino";
 import { terno } from "../pecas/torso/terno";
 import { vestidinho } from "../pecas/torso/vestidinho";
 import { vestidoPadrao } from "../pecas/torso/vestidoPadrao";
@@ -7,11 +9,13 @@ import { vestidoPadrao } from "../pecas/torso/vestidoPadrao";
 export const torso: Parte = {
     imagem: "/images/vestido-padrao.png",
     nome: "Torso",
-    zIndex: 1,
+    zIndex: 2,
     store: torsoStore,
     pecas: [
         vestidoPadrao,
         terno,
         vestidinho,
+        dino,
+        cozinheira
     ]
 }
---
+++ src/roupas/pecas/torso/dino.ts
@@ -0,0 +1,7 @@
+import type { Peca } from "../../../types/peca";
+
+export const dino: Peca = {
+    image: "/images/dino.png",
+    nome: "Dino",
+    sobreposicaoZIndex: 5
+}
