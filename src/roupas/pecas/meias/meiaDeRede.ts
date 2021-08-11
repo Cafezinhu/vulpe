f67f023da60859799a0fe90ec6f30d8d925fcd35
Aerton <aertoncastro@gmail.com>
2021-08-11 16:03:43 -0300

    :bug: M maiúsculo na meia de rede

--- src/roupas/pecas/meias/meiaDeRede.ts
+++ src/roupas/pecas/meias/meiaDeRede.ts
@@ -1,6 +1,6 @@
 import type { Peca } from "../../../types/peca";
 
 export const meiaDeRede: Peca = {
-    image: "/images/meia de rede.png",
+    image: "/images/Meia de rede.png",
     nome: "Meia De Rede"
 }
