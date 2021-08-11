0ce06e37fb50a63a053912a781ac2265014fa2d2
Aerton <aertoncastro@gmail.com>
2021-08-11 15:57:05 -0300

    roupa cozinheira

--- src/roupas/partes/torso.ts
+++ src/roupas/partes/torso.ts
@@ -4,18 +4,22 @@ import { cozinheira } from "../pecas/torso/cozinheira";
 import { dino } from "../pecas/torso/dino";
 import { terno } from "../pecas/torso/terno";
 import { vestidinho } from "../pecas/torso/vestidinho";
+import { vestidoMaid } from "../pecas/torso/vestidoMaid";
 import { vestidoPadrao } from "../pecas/torso/vestidoPadrao";
+import { vestidoValquiria } from "../pecas/torso/vestidoValquiria";
 
 export const torso: Parte = {
     imagem: "/images/vestido-padrao.png",
     nome: "Torso",
-    zIndex: 2,
+    zIndex: 4,
     store: torsoStore,
     pecas: [
         vestidoPadrao,
         terno,
         vestidinho,
         dino,
+        vestidoValquiria,
+        vestidoMaid,
         cozinheira
     ]
 }
--- src/roupas/pecas/torso/cozinheira.ts
+++ src/roupas/pecas/torso/cozinheira.ts
@@ -1,6 +1,6 @@
 import type { Peca } from "../../../types/peca";
 
 export const cozinheira: Peca = {
-    image: "/images/cozinheira-torso.png",
+    image: "/images/roupa-cozinheira.png",
     nome: "Cozinheira"
 }
