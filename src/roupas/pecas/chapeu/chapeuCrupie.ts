f08db65fffaea7acbe6073fe273d83ec50cb66cd
Aerton <aertoncastro@gmail.com>
2021-08-11 22:56:31 -0300

    crupiê

---
+++ src/roupas/pecas/chapeu/chapeuCrupie.ts
@@ -0,0 +1,6 @@
+import type { Peca } from "../../../types/peca";
+
+export const chapeuCrupie: Peca = {
+    image: "/images/chapeu-crupie.png",
+    nome: "Chapéu de Crupie"
+}
---
+++ src/roupas/pecas/torso/roupaCrupie.ts
@@ -0,0 +1,6 @@
+import type { Peca } from "../../../types/peca";
+
+export const roupaCrupie: Peca = {
+    image: '/images/roupa-crupie.png',
+    nome: 'Roupa Crupie',
+}
