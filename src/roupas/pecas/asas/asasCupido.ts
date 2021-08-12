17daa3a22b7618034e1b9bb7cb34ccab8827ef07
Aerton <aertoncastro@gmail.com>
2021-08-11 22:55:26 -0300

    cupido

---
+++ src/roupas/pecas/asas/asasCupido.ts
@@ -0,0 +1,6 @@
+import type { Peca } from "../../../types/peca";
+
+export const asasCupido: Peca = {
+    image: "/images/asas-cupido.png",
+    nome: "Asas de Cupido"
+}
--- src/roupas/pecas/asas/asasValquiria.ts
+++
@@ -1,6 +0,0 @@
-import type { Peca } from "../../../types/peca";
-
-export const asasValquiria: Peca = {
-    image: "/images/asas-valquiria.png",
-    nome: "Asas de Valquíria"
-}
